daaf409934cc3ffeb055d08195ba1b6900135e1a
0724choi <fbfb741@naver.com>
2024-05-21 17:45:38 +0900

    5.21 버블정렬 정의

---
+++ 04_Array_Practice/src/com/kh/practice/run/BubleSortRun.java
@@ -0,0 +1,24 @@
+package com.kh.practice.run;
+
+public class BubleSortRun {
+
+	public static void main(String[] args) {
+		/*int arr[] = {22,99,30,68,87,1,13};
+		int temp = 0; // 위치이동을 위한 임시공간
+		
+		for(int i=0; i<arr.length; i++) { // 버블정렬하는방법
+			for(int j=1; j<arr.length; j++) {
+				if(arr[j-1] > arr[j]) { // 앞의 숫자가 뒤의 숫자보다 큰 경우
+					 temp = arr[j-1]; // temp에 앞에숫자보관
+					 arr[j-1] = arr[j]; // 더 작은 숫자를 앞자리에 넣기 (22, 30 , 30 ... 인 상황
+					 arr[j] = temp; // temp에 보관했던 숫자를 뒷자리에 넣기 (22, 99 ,30 위치이동 완료
+				}
+			}
+		}
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+ " "); // 버블정렬
+		}*/
+
+	}
+
+}
