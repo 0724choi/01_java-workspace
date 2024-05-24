001a315ad2105a7ad96a6b8752911168e1826634
0724choi <fbfb741@naver.com>
2024-05-24 17:53:56 +0900

    5.24 과제

---
+++ 06_Homework/src/com/kh/practice/student/view/StudentMenu.java
@@ -0,0 +1,29 @@
+package com.kh.practice.student.view;
+
+import com.kh.practice.student.model.vo.Student;
+
+public class StudentMenu {
+	
+	// 생성자부
+	public StudentMenu() {}
+	public StudentMenu(Student[] sArr, int sum, double[] avgScore) {
+		System.out.println("=========학생정보출력============");
+		System.out.println(sArr);
+		System.out.println("=========학생성적출력============");
+		System.out.println(sum);
+		System.out.println(avgScore);
+		System.out.println("=========학생정보출력============");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
